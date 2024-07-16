bc56fcb3b023544273a91ec1215d33e66797cb5a
Sergei Iakhnitskii <serjflint@gmail.com>
2024-07-16 20:38:02 +0000

    feat: add granian asgi server

---
+++ dev.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+uvicorn src.random_pycon_2024_bot.app:app --reload --host 0.0.0.0
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-uvicorn src.random_pycon_2024_bot.app:app --reload --host 0.0.0.0
+granian --interface asgi src.random_pycon_2024_bot.app:app --reload --host 0.0.0.0
