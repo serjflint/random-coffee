f39d675de4aca388e3ffd0324e7d5425e15d4045
Sergei Iakhnitskii <serjflint@gmail.com>
2024-07-18 06:53:00 +0000

    refactor: simplify user

--- src/random_pycon_2024_bot/db.py
+++ src/random_pycon_2024_bot/db.py
@@ -4,21 +4,25 @@ from random_pycon_2024_bot import messages
 from random_pycon_2024_bot.utils import notnull
 
 
+def get_user(user_id: int, context: te.ContextTypes.DEFAULT_TYPE) -> dict:
+    ctx = notnull(context.user_data)
+    users = ctx.setdefault('users', {})
+    return users.setdefault(user_id, {})
+
+
 def get_message(msg_code: str, user_id: int, context: te.ContextTypes.DEFAULT_TYPE) -> str:
-    lang_code = notnull(context.user_data).setdefault('lang_code', {}).setdefault(user_id, 'ru')
+    user = get_user(user_id, context)
+    lang_code = user.setdefault('lang_code', 'ru')
     return messages.MESSAGES.get(msg_code, {}).get(lang_code) or messages.UNKNOWN_TEXT_MESSAGE_RU
 
 
 def register(user_id: int, context: te.ContextTypes.DEFAULT_TYPE) -> None:
-    ctx = notnull(context.user_data)
-    users = ctx.setdefault('users', {})
-    data = users.setdefault(user_id, {})
-    data['enabled'] = True
+    user = get_user(user_id, context)
+    user['enabled'] = True
 
 
 def unregister(user_id: int, context: te.ContextTypes.DEFAULT_TYPE) -> None:
-    ctx = notnull(context.user_data)
-    users = ctx.setdefault('users', {})
-    if user_id not in users:
+    user = get_user(user_id, context)
+    if not user:
         return
-    users[user_id]['enabled'] = False
+    user['enabled'] = False
